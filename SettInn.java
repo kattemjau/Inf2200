c2409957f432e69c29c5f17ccd94b280eb823eb8
kattemjau <sindre.c.h@hotmail.com>
2016-09-03 17:00:20 +0200

    , and an empty message aborts the commit.

--- SettInn.java
+++ SettInn.java
@@ -1,29 +1,65 @@
 class SettInn{
+	Node root;
 
-  void insert(Node in){
+	class Node<String extends Comparable<String>>{
+		//hoyere==horyre
+		Node hoyere;
+		//lavere==venstre
+		Node lavere;
+		//slik at man kan gå tilbake
+		Node bak;
+		//ordet til noden
+		String data;
 
-  }
-  Node search(String in){
-    return null;
-  }
-  void deleate(Node in){
+		Node(String data){
+			this.data=data;
+		}
+		int compareTo(Node in){
+			return this.compareTo(in);
+		}
+	}
 
-  }
+
+
+	void insert(Node in){
+		Node temp = root;
+		while(true){
+			if(temp.compareTo(in) <0){
+				temp=temp.lavere;
+			}
+			else if(temp.compareTo(in)>0){
+				temp=temp.hoyere;
+			}
+		}
+		
+	}
+	Node search(String in){
+		Node temp = root;
+		while(true){
+			if(temp.compareTo(in) <0){
+				if(temp.lavere != null){
+					temp=temp.lavere;
+				}else{
+					return null;
+				}
+			}else if(temp.compareTo(in)>0){
+				if(temp.hoyere != null){
+					temp=temp.hoyere;
+				}else{
+					return null;
+				}
+			}else{
+				continue;
+			}
+		}
+		return temp;
+	}
+	void deleate(String in){
+		Node temp = search(in);
+
+
+	}
+
 }
 
-class Node<String extends Comparable<String>>{
-  public Node hoyere;
-  public Node lavere;
-  public Node bak;
 
-  public String data;
-
-  Node(String data){
-    this.data=data;
-  }
-  boolean compareTo(Node in){
-    return data.equals(in.data);
-  }
-
-
-}
