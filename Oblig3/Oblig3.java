db5583b197ded22db609deffe86245d0e2b3a9d8
kattemjau <sindre.c.h@hotmail.com>
2017-10-21 14:49:01 +0200

    might work

--- Oblig3/Oblig3.java
+++ Oblig3/Oblig3.java
@@ -19,13 +19,10 @@ public class Oblig3{
 	public static void main(String[] args) {
 	new Oblig3().accumulating(Integer.parseInt(args[0]));
 
-
-
 	}
 	public void accumulating(int len){
 		a=new int[len];
 		n=len;
-		ARLSort vrdx = new ARLSort();
 		Random r = new Random(123);
 		for (int i =0; i < len;i++) {	//generating Random array
 			 a[i] = r.nextInt(len);
@@ -37,86 +34,129 @@ public class Oblig3{
 
 
 		long t = System.nanoTime();
-		vRadix(a);
+		a=vRadix(a);
 		double tim = (System.nanoTime() -t)/ (double)1000000.0;
 		System.out.println("VenstreRadix Sorterte "+n+" tall paa:" + tim + "millisek.");
 		testSort(a);
-		System.out.println(" ");
-
-
-		long tt = System.nanoTime();
-		Arrays.sort(aClone);
-		double tid = (System.nanoTime() -tt)/ (double)1000000.0;
-		System.out.println("Array.sort Sorterte "+n+" tall paa:" + tid + "millisek.");
-		testSort(aClone);
+		// System.out.println(" ");
+		//
+		//
+		// long tt = System.nanoTime();
+		// Arrays.sort(aClone);
+		// double tid = (System.nanoTime() -tt)/ (double)1000000.0;
+		// System.out.println("Array.sort Sorterte "+n+" tall paa:" + tid + "millisek.");
+		// testSort(aClone);
 
 	}
 	int[] vRadix(int[] a){
 		int max = a[0], numBit = 2, numDigits, n =a.length;
-
-		int [] bit ;
+		//
+		// int [] bit ;
 
 	 // a) finn max verdi i a[]
-	 // b) bestem numBit = høyeste (mest venstre) bit i ‘max’ som ==1
+	//  // b) bestem numBit = høyeste (mest venstre) bit i ‘max’ som ==1
 		for (int i = 1 ; i < n ; i++)
 			 if (a[i] > max) max = a[i];
 		while (max >= (1L<<numBit) )numBit++; // antall binaere siffer i max
+	 //
+	// 	// bestem antall bit i numBits sifre
+	// 	numDigits = Math.max(1, numBit/7);
+	// 	bit = new int[numDigits];
+	// 	int rest = (numBit%numDigits), sum =0;;
+	 //
+	// 	// fordel bitene vi skal sortere paa jevnt
+	// 	for (int i = 0; i < bit.length; i++){
+	// 		bit[i] = numBit/numDigits;
+	// 			if ( rest-- > 0)  bit[i]++;
+	// 	}
 
-		// bestem antall bit i numBits sifre
 		numDigits = Math.max(1, numBit/7);
-		bit = new int[numDigits];
-		int rest = (numBit%numDigits), sum =0;;
-
-		// fordel bitene vi skal sortere paa jevnt
-		for (int i = 0; i < bit.length; i++){
-			bit[i] = numBit/numDigits;
-				if ( rest-- > 0)  bit[i]++;
-		}
-
 
 		// c) Første kall (rot-kallet) på VenstreRadix med a[], b[] , numBit, og lengden av første siffer
-		radx(a, bit[0], 0);
+		// n=100 skal kjøre 3 ganger
+		// starter på bit plass 2 så 1 så 0
+		int len =numBit/numDigits;
+		//finne ut hvor mange ganger den må gå bassert på max og mask
+		radx(a, len, 0);	//a, antall bits den skal skifte med, posisjon
 
-		return a;
+		return radx(a, len, 0);
 
 
 
 	}
-	int[] radx(int[] a, int maskLen, int shift){
-		int value=0, j, n = a.length;
-	  int mask = (1<<maskLen) -1;
-		int[] count = new int [mask+1];
+	int[] radx(int[] a, int len, int pos){
 		int[] b = new int [a.length];
+		int value=0, j, n = a.length;
+		int mask = (1<<len) -1;
+		int[] count = new int [mask+1];
 
+		System.out.println("Math: " + Math.pow(len, pos));
+		// System.out.println("len_ " + len);
+		System.out.println("mask: " + mask);
 
+		//count instances of eatch number
 		// d) count[] =hvor mange det er med de ulike verdiene
 		// av dette sifret I a [left..right]
 		for (int i = 0; i < n; i++) {
-			count[(a[i]>>> shift) & mask]++; //gjør om til å gå fra venstre til høyre
+			count[(a[i]/(int) Math.pow(10, pos)) & mask]++; //gjør om til å gå fra venstre til høyre
 		}
 
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(a[i] +  " == " + ((a[i]/(int) Math.pow(10, pos)) & mask));
+		}
 		// e) Tell opp verdiene I count[] slik at count[i] sier hvor i b[] vi skal
 		// flytte første element med verdien ‘i’ vi sorterer.
+
 		for (int i = 0; i <= mask; i++) {
 			j = count[i];
 			count[i] = value;
 			value += j;
 		}
-		// f) Flytt tallene fra a[] til b[] sorter på dette sifferet I a[left..right] for
-		// alle de ulike verdiene for dette sifferet
+
 		for (int i = 0; i < n; i++) {
-			b[count[(a[i]>>>shift) & mask]++] = a[i]; //endre til å flytte bitsa fra venstre
+			b[count[(a[i]/(int) Math.pow(10, pos)) & mask]++] = a[i]; //endre til å flytte bitsa fra venstre
 		}
-		// g) Kall enten innstikkSort eller rekursivt VenstreRadix
-		// på neste siffer (hvis vi ikke er ferdige) for alle verdiene vi har av nåværende siffer
-		// if(shift!=lastByte){
-		// 	radx(b, leftSortBit, maskLen);
-		// }
-		// Vurder når vi. skal kopiere tilbake b[] til a[] ??
-		return b;
 
-
+		//breaks if it reaches last run
+		if(pos==0){
+			return b;
+		}
+		return radx(a, len, pos-1);	//endre a til b
+		// return b;
 	}
+	// int value=0, j, n = a.length;
+	// int mask = (1<<maskLen) -1;
+	// int[] count = new int [mask+1];
+	// int[] b = new int [a.length];
+	//
+	//
+	// // d) count[] =hvor mange det er med de ulike verdiene
+	// // av dette sifret I a [left..right]
+	// for (int i = 0; i < n; i++) {
+	// 	count[(a[i]>>> shift) & mask]++; //gjør om til å gå fra venstre til høyre
+	// }
+	//
+	// // e) Tell opp verdiene I count[] slik at count[i] sier hvor i b[] vi skal
+	// // flytte første element med verdien ‘i’ vi sorterer.
+	// for (int i = 0; i <= mask; i++) {
+	// 	j = count[i];
+	// 	count[i] = value;
+	// 	value += j;
+	// }
+	// // f) Flytt tallene fra a[] til b[] sorter på dette sifferet I a[left..right] for
+	// // alle de ulike verdiene for dette sifferet
+	// for (int i = 0; i < n; i++) {
+	// 	b[count[(a[i]>>>shift) & mask]++] = a[i]; //endre til å flytte bitsa fra venstre
+	// }
+	// // g) Kall enten innstikkSort eller rekursivt VenstreRadix
+	// // på neste siffer (hvis vi ikke er ferdige) for alle verdiene vi har av nåværende siffer
+	// // if(shift!=lastByte){
+	// // 	radx(b, leftSortBit, maskLen);
+	// // }
+	// // Vurder når vi. skal kopiere tilbake b[] til a[] ??
+	// return b;
+	//
 	// /** N.B. Sorterer a[] stigende – antar at: 0 ≤ a[i]) < 232 , returnerer tiden i millisek. */
 	// final static int NUM_BIT =9; // eller: 6-13 er kanskje best.. finn selv ut hvilken verdi som er raskest
 	// final static int MIN_NUM = 31; // mellom 16 og 60, kvikksort bruker 47
